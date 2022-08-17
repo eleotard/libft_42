0a5382743af009cb4374f03f0225bbd4646a8fe2
elsie <eleotard@student.42.fr>
2022-08-17 16:59:26 +0200

    sans invalid read sur strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eleotard <eleotard@student.42.fr>          +#+  +:+       +#+        */
+/*   By: elsie <elsie@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 14:25:02 by eleotard          #+#    #+#             */
-/*   Updated: 2022/04/11 16:17:35 by eleotard         ###   ########.fr       */
+/*   Updated: 2022/08/17 16:58:03 by elsie            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	if (!n)
 		return (0);
 	i = 0;
-	while ((s1[i] || s2[i]) && (i < n - 1))
-	{
-		if (s1[i] != s2[i])
-			break ;
+	while ((s1[i] && s2[i]) && (s1[i] == s2[i]) && (i < n - 1))
 		i++;
-	}
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 /*#include <string.h>
